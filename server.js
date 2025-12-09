b41c19033f7f4c2bd13bb3d2b1e0f7df4b583573
bijdey <creativenetwork.bijay@gmail.com>
2025-12-09 12:00:28 +0530

    addes session and cookies

--- server.js
+++ server.js
@@ -3,23 +3,44 @@ const express = require('express');
 const dotenv = require('dotenv');
 const http = require('http');
 const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 // Initialize App
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Session Setup
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "supersecretbijaykey123",
+        resave: false, 
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,          // Protects from JS access
+            maxAge: 1000 * 60 * 60,  // 1 hour session
+            secure: false            // true only if using HTTPS
+        }
+    })
+);
+
 // View Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Static Files 
+// Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use('/', require('./routes/mainRoutes'));
 
-// 404 Page (Render directly without and route and controller to the 404page)
+// 404
 app.use((req, res) => {
     res.status(404).render("pages/404error");
 });
